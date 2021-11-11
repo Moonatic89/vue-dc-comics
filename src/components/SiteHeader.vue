7758c3e73471db90c4425109b7531a491b986689
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-11 17:57:28 +0100

    Working on header

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="SiteHeader">
+    <nav>
+      <div class="logo">
+        <img src="../assets/dc-logo.png" alt="" />
+      </div>
+      <ul>
+        <li>CHARACTERS</li>
+        <li class="bottomLine">COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+.SiteHeader {
+  height: 118px;
+
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+  }
+
+  li {
+    padding: 0 1rem;
+    line-height: 118px;
+  }
+  .bottomLine {
+    border-bottom: 2px solid blue;
+  }
+}
+</style>
